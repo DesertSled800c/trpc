520168c978747f03db38334ba7d585da6620721d
Akshay Khona <DesertSled800c@gmail.com>
2023-08-30 13:28:00 -0500

    consolelog removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,6 @@ import { PostView } from "~/components/postview";
 const CreatePostWizard = () => {
   const { user } = useUser();
 
-  console.log(user);
-
   const [input, setInput] = useState("");
 
   const ctx = api.useContext();
