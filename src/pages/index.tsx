c60cb63767813ce8c6ef03e6559f66fb9ba3bf62
Akshay Khona <DesertSled800c@gmail.com>
2023-08-31 16:01:47 -0500

    Sign out button added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import { useUser } from "@clerk/clerk-react";
-import { SignInButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  UserButton,
+  useUser,
+  SignOutButton,
+} from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
-import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
@@ -36,12 +39,15 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
       />
       <input
         placeholder="Type some emojis!"
@@ -63,7 +69,7 @@ const CreatePostWizard = () => {
         <button onClick={() => mutate({ content: input })}>Post</button>
       )}
       {isPosting && (
-        <div className="flex justify-center">
+        <div className="flex items-center justify-center">
           <LoadingSpinner size={20} />
         </div>
       )}
@@ -71,13 +77,17 @@ const CreatePostWizard = () => {
   );
 };
 
-
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (postsLoading) return <LoadingPage />;
+  if (postsLoading)
+    return (
+      <div className="flex grow">
+        <LoadingPage />
+      </div>
+    );
 
-  if (!data) return <div>Something went wrong!!</div>;
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <div className="flex grow flex-col overflow-y-scroll">
@@ -87,30 +97,39 @@ const Feed = () => {
     </div>
   );
 };
+
 const Home: NextPage = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
 
   // Start fetching asap
   api.posts.getAll.useQuery();
 
-  // Return empty div if BOTH aren't loaded, since user tends to load faster
-
+  // Return empty div if user isn't loaded
   if (!userLoaded) return <div />;
 
   return (
-    <>
-      <PageLayout>
-        <div className="border-b border-slate-400 p-4">
-          {!isSignedIn && (
-            <div className="flex justify-center">
-              <SignInButton />
-            </div>
-          )}
-          {isSignedIn && <CreatePostWizard />}
-        </div>
-        <Feed />
-      </PageLayout>
-    </>
+    <PageLayout>
+      <div className="flex border-b border-slate-400 p-4">
+        {!isSignedIn && (
+          <div className="flex justify-center">
+            <SignInButton />
+          </div>
+        )}
+        {isSignedIn && <CreatePostWizard />}
+      </div>
+
+      <Feed />
+      <div className="flex items-center justify-between p-4 text-xl">
+        <a href="https://github.com/DesertSled800c">
+          <div className="flex items-center justify-center gap-2">
+            <div>Github</div>
+          </div>
+        </a>
+        <span>
+          <SignOutButton />
+        </span>
+      </div>
+    </PageLayout>
   );
 };
 
